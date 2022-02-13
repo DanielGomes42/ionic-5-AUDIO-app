f7d44c84db4cc3503e1f42f71b56fb9965a3d56c
danielgomes42 <damsilva757@gmail.com>
2022-02-13 17:26:53 -0300

    Upgrade

--- src/app/album/album.page.ts
+++ src/app/album/album.page.ts
@@ -8,20 +8,31 @@ import albums from '../../assets/mockdata//albums';
   styleUrls: ['./album.page.scss'],
 })
 export class AlbumPage implements OnInit {
-  data = null;
+  album: any;
+  data = {
+    backgroundColor: 'darkred',
+  };
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    const title = this.activatedRoute.snapshot.paramMap.get('title');
+    /*const title = this.activatedRoute.snapshot.paramMap.get('title');
     const decodedTitle = decodeURIComponent(title);
-    this.data = albums[decodedTitle];    
+    this.data = albums[decodedTitle];  */
+
+    this.album = history.state.album;
+
+    // ----> this.album.link
   }
 
-    // Helper function for image names
-    dasherize(string) {
-      return string.replace(/[A-Z]/g, function(char, index) {
-        return (index !== 0 ? '-' : '') + char.toLowerCase();
-      });
-    };
+  getLink(album): void {
+    // ----> console.log(album.link)
+  }
+
+  // Helper function for image names
+  dasherize(string) {
+    return string.replace(/[A-Z]/g, function (char, index) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,38 +12,38 @@ import lista3 from '../../assets/mockdata/lista3.json';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  data = [
-    {
-      title: 'Musicas...',
-      albums: lista1
-    },
-  ];
-
-  
+  albums: Array<any> = []
 
   opts = {
     slidesPerView: 0.9,
     slidesOffsetBefore: 20,
     spaceBetween: 20,
     freeMode: true
-  };
+  }
   
-  constructor(private router: Router) {
-    
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.albums = lista1
+
+    console.log(this.albums)
   }
 
-  openAlbum(album) {
-    const titleEscaped = encodeURIComponent(album.title);
-    this.router.navigateByUrl(`/tabs/tab1/${titleEscaped}`)[0];
+  openAlbum(album: any): void {
+    /*const titleEscaped = encodeURIComponent(album.title);
+    this.router.navigateByUrl(`/tabs/tab1/${titleEscaped}`)*/
+
+    this.router.navigate([`${this.router.url}/${album.id}`], {
+      state: { album }
+    })
+
+  
   }
-  
-  
 
   // Helper function for image names
   dasherize(string) {
     return string.replace(/[A-Z]/g, function(char, index) {
-      return (index !== 0 ? '-' : '') + char.toLowerCase();
-    });
-   };
+      return (index !== 0 ? '-' : '') + char.toLowerCase()
+    })
+   }
 }
