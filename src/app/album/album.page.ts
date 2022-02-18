441b54cef38892736a0759ddcc6cc80e46fd1caa
danielgomes42 <damsilva757@gmail.com>
2022-02-18 04:53:01 -0300

    album.lin

--- src/app/album/album.page.ts
+++ src/app/album/album.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import albums from '../../assets/mockdata//albums';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 
 @Component({
   selector: 'app-album',
@@ -13,20 +14,21 @@ export class AlbumPage implements OnInit {
     backgroundColor: 'darkred',
   };
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute ,private nativeAudio: NativeAudio) { }
 
   ngOnInit() {
     /*const title = this.activatedRoute.snapshot.paramMap.get('title');
     const decodedTitle = decodeURIComponent(title);
     this.data = albums[decodedTitle];  */
-
+    this.nativeAudio.preloadSimple('01', '{{album.link}}');
     this.album = history.state.album;
 
     // ----> this.album.link
   }
 
   getLink(album): void {
-    console.log(album.link)
+    this.nativeAudio.play('01', () => console.log('uniqueId1 is done playing'));
+    // console.log(album.link)
   }
 
   // Helper function for image names
